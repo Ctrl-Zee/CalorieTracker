6322b5d4ba7739a53d59b8e2bd62c88df551cb12
Andrew Smith <andrew.smith@moserit.com>
2024-10-29 14:59:20 -0400

    Added Gluestack

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import { Text, View } from "react-native";
+import { Box } from "@/components/ui/box";
 
 export default function HomeScreen() {
   return (
     <View className="bg-red-100 p-7">
       <Text>Home Screen</Text>
+      <Box className="bg-tertiary-400 p-5">
+        <Text className="text-typography-0">This is the Box</Text>
+      </Box>
     </View>
   );
 }
